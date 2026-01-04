789f109b3dc249c71f7030ab72d358c88155c444
ramchavan00001 <ramchavan00001@gmail.com>
2026-01-04 11:37:15 +0530

    updated the remove duplicates from sorted array - used stream to print the element of array

--- src/main/java/BestInterviewExamaples/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/BestInterviewExamaples/RemoveDuplicatesFromSortedArray.java
@@ -1,5 +1,6 @@
 package BestInterviewExamaples;
 
+import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -26,7 +27,33 @@ public class RemoveDuplicatesFromSortedArray {
         
         System.out.println("The sorted array"+sorted);
 		
+        anotherApproachUsingTwoPointer();
 		
 	}
+	
+	public static void anotherApproachUsingTwoPointer()
+	{
+		int [] arr= {0,0,1,1,1,2,3,3,5};
+		
+		int j=1; //this counter is used to return number of unique element
+		//as the first element in array is always unique so will start from second element
+		
+		for(int i=1;i<=arr.length-1;i++)
+		{
+			if(arr[i]!=arr[i-1])
+			{
+				arr[j]=arr[i];
+				j++;
+			}
+		}
+		System.out.println("Number of unique element is: "+j);
+		
+		
+		System.out.println("Array after removing duplicate element");
+		
+		Arrays.stream(arr).forEach(System.out::println);
+		
+	}
+	
 	
 }
